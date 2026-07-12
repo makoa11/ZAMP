a1e8c4e7a6f17dfd7039a67b525fff3793b25324
DarshanJain-07 <jaindarshan1005@gmail.com>
2026-07-12 11:13:43 +0530

    removed accidental commits

--- test_dimensions.py
+++
@@ -1,5 +0,0 @@
-import pdfplumber
-
-with pdfplumber.open("/home/darshan/Desktop/ZAMP/mail-invoice-1-overlay.pdf") as pdf:
-    for page in pdf.pages:
-        print(f"Page {page.page_number}: width={page.width}, height={page.height}")
